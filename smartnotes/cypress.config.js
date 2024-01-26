f74c6d73490d372c689537fb37176c7757cfcf2c
noordeen123 <noordeen.cse_c2019@crescent.education>
2024-01-26 17:22:22 +0530

    fix

--- smartnotes/cypress.config.js
+++ smartnotes/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: 'http://127.0.0.1:8000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
